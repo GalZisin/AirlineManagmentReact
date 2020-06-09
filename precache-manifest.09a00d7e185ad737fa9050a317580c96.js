51b0ff6d7a1bd2ee00c210043247daaf3672fd9f
Gal Zisin <galzisin86@gmail.com>
2020-06-09 19:56:08 +0300

    Updates

---
+++ precache-manifest.09a00d7e185ad737fa9050a317580c96.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4553a3ea813b0e1acaf85ee8987cf58a",
+    "url": "/AirlineManagmentReact/index.html"
+  },
+  {
+    "revision": "9a4e9f710429de4a395f",
+    "url": "/AirlineManagmentReact/static/css/2.b561f01f.chunk.css"
+  },
+  {
+    "revision": "98a7baaf714ba4c9cff0",
+    "url": "/AirlineManagmentReact/static/css/main.aa827afe.chunk.css"
+  },
+  {
+    "revision": "9a4e9f710429de4a395f",
+    "url": "/AirlineManagmentReact/static/js/2.f648ed05.chunk.js"
+  },
+  {
+    "revision": "6b7aba620a56cfa60666415e067b2a45",
+    "url": "/AirlineManagmentReact/static/js/2.f648ed05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a7baaf714ba4c9cff0",
+    "url": "/AirlineManagmentReact/static/js/main.fdf867b2.chunk.js"
+  },
+  {
+    "revision": "c9eb0aa24c58486ef6ea",
+    "url": "/AirlineManagmentReact/static/js/runtime-main.ff90a1f4.js"
+  },
+  {
+    "revision": "3daa7d2ae9828ab0df317f593210938a",
+    "url": "/AirlineManagmentReact/static/media/Aerobell Airlines.3daa7d2a.jpg"
+  },
+  {
+    "revision": "561acc30453ae217e69d0d933956a90b",
+    "url": "/AirlineManagmentReact/static/media/Air Mali.561acc30.jpg"
+  },
+  {
+    "revision": "799971be78c36bd2b2b20df52e52349e",
+    "url": "/AirlineManagmentReact/static/media/Aircraft_minimalistic_gray_planes_simple_colors_2560x1440.799971be.jpg"
+  },
+  {
+    "revision": "4574f4d0103cfa7ea0fae38098050f74",
+    "url": "/AirlineManagmentReact/static/media/Corendon Airlines.4574f4d0.jpg"
+  },
+  {
+    "revision": "ce51b3dae9d7a818bf34d8202ccd2461",
+    "url": "/AirlineManagmentReact/static/media/El-AL.ce51b3da.jpg"
+  },
+  {
+    "revision": "d9a9fb4b1af8f3e894da4dcfc69a04c1",
+    "url": "/AirlineManagmentReact/static/media/FlyMontserrat.d9a9fb4b.jpg"
+  },
+  {
+    "revision": "bafc1a9cf6aababff263c5853652b547",
+    "url": "/AirlineManagmentReact/static/media/Heli Air Monaco.bafc1a9c.jpg"
+  },
+  {
+    "revision": "43d2cd5be99ed0ceeab449ccdaa29922",
+    "url": "/AirlineManagmentReact/static/media/Helitours.43d2cd5b.jpg"
+  },
+  {
+    "revision": "fb0a27d39bf8084d887ea72c14e92cf7",
+    "url": "/AirlineManagmentReact/static/media/Lions Air.fb0a27d3.jpg"
+  },
+  {
+    "revision": "66f42a7ec9d42783376b6131444878dc",
+    "url": "/AirlineManagmentReact/static/media/Malawian Airlines.66f42a7e.jpg"
+  },
+  {
+    "revision": "fa9eaa69d209c6b167164db1f96a5226",
+    "url": "/AirlineManagmentReact/static/media/SVG Air.fa9eaa69.jpg"
+  },
+  {
+    "revision": "9977d6df3e45e5950316c3fd1df57923",
+    "url": "/AirlineManagmentReact/static/media/Saurya Airlines.9977d6df.jpg"
+  },
+  {
+    "revision": "a2ca25e54678ff52e67270fc7482e089",
+    "url": "/AirlineManagmentReact/static/media/Sky Shuttle Helicopters.a2ca25e5.jpg"
+  },
+  {
+    "revision": "7d3204eb59b36ab16ee344e7024dfb1b",
+    "url": "/AirlineManagmentReact/static/media/SkyWay.7d3204eb.jpg"
+  },
+  {
+    "revision": "4a3f083b7145cdfe26ee9c04dc84b378",
+    "url": "/AirlineManagmentReact/static/media/Solomon Islands Seaplanes.4a3f083b.jpg"
+  },
+  {
+    "revision": "18ebe6c1899aef3189e48d5a1d4b4cc6",
+    "url": "/AirlineManagmentReact/static/media/Summit Helicopters.18ebe6c1.jpg"
+  },
+  {
+    "revision": "aed0dc50de0a6a1e8e4b00925e4c1149",
+    "url": "/AirlineManagmentReact/static/media/Trans Am Aero Express del Ecuador.aed0dc50.jpg"
+  },
+  {
+    "revision": "45a0dbecfd358ab0627b2a588034e948",
+    "url": "/AirlineManagmentReact/static/media/Turkish Airlines.45a0dbec.jpg"
+  },
+  {
+    "revision": "f01e5359b17ce48054d6d16b6a259402",
+    "url": "/AirlineManagmentReact/static/media/Ulendo Airlink.f01e5359.jpg"
+  },
+  {
+    "revision": "9c7febef66beb5da39f0b3f0ec5d536c",
+    "url": "/AirlineManagmentReact/static/media/Vanuatu Helicopters.9c7febef.jpg"
+  },
+  {
+    "revision": "03b0bd21d1e1946e2832121bbde38ea2",
+    "url": "/AirlineManagmentReact/static/media/logo3.03b0bd21.png"
+  },
+  {
+    "revision": "e614d3168ebd77a568f6e100ec262940",
+    "url": "/AirlineManagmentReact/static/media/para.e614d316.jpg"
+  },
+  {
+    "revision": "6553c0b44e00ce17b58ebd7105e0f0dd",
+    "url": "/AirlineManagmentReact/static/media/white-clouds-background.6553c0b4.jpg"
+  }
+]);
